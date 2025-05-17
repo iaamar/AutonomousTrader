2dcef777febdf9311f5ef05b720f7bdbefb7d1b3
Amar Nagargoje <amargithub@gmail.com>
2025-05-17 09:22:43 -0400

    add traders mcp tools, db and markekt server, client

--- mcp_params copy.py
+++ mcp_params copy.py
@@ -28,6 +28,6 @@ trader_mcp_server_params = [
 def researcher_mcp_server_params(name: str):
     return [
         {"command": "uvx", "args": ["mcp-server-fetch"]},
-        {"command": "npx", "args": ["-y", "@modelcontextprotocol/server-brave-search"], "env": {"BRAVE_API_KEY": os.getenv("BRAVE_API_KEY")}},
+        {"command": "npx", "args": ["-y", "@modelcontextprotocol/server-brave-search"], "env": brave_env},
         {"command": "npx", "args": ["-y", "mcp-memory-libsql"], "env": {"LIBSQL_URL": f"file:./memory/{name}.db"}}
     ]
